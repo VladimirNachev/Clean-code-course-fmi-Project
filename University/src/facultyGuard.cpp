600f48cb83794db06d205529e31da295e4ad23e3
Vladimir Nachev <vladimir.jivkov@gmail.com>
2018-07-04 23:50:02 +0100

    Create FacultyGuard class source file

---
+++ University/src/facultyGuard.cpp
@@ -0,0 +1,47 @@
+#include "../include/facultyGuard.h"
+#include "../include/employee.h"
+#include<iostream>
+#include<cassert>
+
+using namespace std;
+
+const int FACULTY_GUARD_ANNUAL_SALARY_INCREASE = 150;
+
+FacultyGuard::FacultyGuard(const char* name, const char* address, int birthYear, int experience, double salary, bool isEligibleToWearGun) :
+  Employee(name, address, birthYear, experience, salary)
+{
+  this->isEligibleToWearGun = isEligibleToWearGun;
+}
+
+FacultyGuard::FacultyGuard(const FacultyGuard &otherFacultyGuard) : Employee(otherFacultyGuard)
+{
+  this->isEligibleToWearGun = otherFacultyGuard.isEligibleToWearGun;
+}
+
+FacultyGuard::~FacultyGuard() { }
+
+FacultyGuard& FacultyGuard::operator=(const FacultyGuard &otherFacultyGuard)
+{
+  if (this != &otherFacultyGuard)
+  {
+    Employee::operator=(otherFacultyGuard);
+    this->isEligibleToWearGun = otherFacultyGuard.isEligibleToWearGun;
+  }
+
+  return *this;
+}
+
+bool FacultyGuard::getGunEligibilityStatus() const
+{
+  return isEligibleToWearGun;
+}
+
+void FacultyGuard::setGunEligibilityStatus(bool isEligibleToWearGun)
+{
+  this->isEligibleToWearGun = isEligibleToWearGun;
+}
+
+void FacultyGuard::increaseAnnualSalary()
+{
+  salary += FACULTY_GUARD_ANNUAL_SALARY_INCREASE;
+}

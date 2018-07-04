a77cc54468f27fbd65d4ce356c394888d802e683
Vladimir Nachev <vladimir.jivkov@gmail.com>
2018-07-04 23:49:05 +0100

    Create FacultyGaurd class header file

---
+++ University/include/facultyGuard.h
@@ -0,0 +1,23 @@
+#ifndef FacultyGuard_h
+#define FacultyGuard_h
+#include "./employee.h"
+
+class FacultyGuard : public Employee
+{
+public:
+  FacultyGuard(const char*, const char*, int, int, double, bool);
+  FacultyGuard(const FacultyGuard&);
+  ~FacultyGuard();
+  FacultyGuard& operator=(const FacultyGuard&);
+
+  bool getGunEligibilityStatus() const;
+
+  void setGunEligibilityStatus(bool);
+
+  void increaseAnnualSalary();
+
+private:
+  bool isEligibleToWearGun;
+};
+
+#endif // FacultyGuard_h

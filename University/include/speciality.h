4008d662539036792f3c9984d4bbf51bc51cd3e4
Vladimir Nachev <vladimir.jivkov@gmail.com>
2018-07-05 15:14:51 +0100

    Create Speciality class header file

---
+++ University/include/speciality.h
@@ -0,0 +1,38 @@
+#ifndef Speciality_h
+#define Speciality_h
+
+class Speciality
+{
+public:
+  Speciality(const char*, int, int, char**);
+  Speciality(const Speciality&);
+  ~Speciality();
+  Speciality& operator=(const Speciality&);
+
+  char* getName() const;
+  int getEnrolledPeopleMaxCount() const;
+  int getDisciplinesCount() const;
+  char** getDisciplines() const;
+
+  void setName(const char*);
+  void setEnrolledPeopleMaxCount(int);
+  void setDisciplines(int, char**);
+
+  void addDiscipline(const char*);
+  void removeDiscipline(const char*);
+
+private:
+  char* name;
+  int enrolledPeopleMaxCount;
+  int disciplinesCount;
+  char** disciplines;
+
+  void copySpeciality(const Speciality&);
+  void deleteSpeciality();
+  void checkDisciplinesLength(int, char**);
+  void copyDisciplines(int, char**);
+  void redirectDisciplinesPointers(int);
+  void copyDynamicMemoryString(char*&, const char*);
+};
+
+#endif // Speciality_h

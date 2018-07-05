e12a692d5278a1c3e0df5ba85eb7b7d47ecd37bc
Vladimir Nachev <vladimir.jivkov@gmail.com>
2018-07-05 09:21:37 +0100

    Add tests for FacultyGuard class and change .cbp file

---
+++ University/test/facultyGuard.cpp
@@ -0,0 +1,34 @@
+#include "../include/catch.hpp"
+#include "../include/facultyGuard.h"
+#include<cstring>
+
+#include<iostream>
+using namespace std;
+
+TEST_CASE("FacultyGuard class - faculty guard can be created")
+{
+  FacultyGuard facultyGuard("Dimitar Ivanov", "Varna", 1965, 35, 1777.7, true);
+  REQUIRE(strcmp(facultyGuard.getName(), "Dimitar Ivanov") == 0);
+}
+
+bool areSalariesEqual(double firstNumber, double secondNumber)
+{
+  return firstNumber - secondNumber > -0.0001 && firstNumber - secondNumber < 0.0001;
+}
+
+TEST_CASE("FacultyGuard class - faculty guard's salary can be annually increased")
+{
+  const int FACULTY_GUARD_ANNUAL_SALARY_INCREASE = 150;
+  FacultyGuard facultyGuard("Dimitar Ivanov", "Varna", 1965, 35, 1777.7, true);
+  const double salaryBeforeIncrease = facultyGuard.getSalary();
+  facultyGuard.increaseAnnualSalary();
+  const double salaryAfterIncrease = facultyGuard.getSalary();
+  const double expectedSalary = salaryBeforeIncrease + FACULTY_GUARD_ANNUAL_SALARY_INCREASE;
+  REQUIRE(areSalariesEqual(salaryAfterIncrease, expectedSalary));
+}
+
+TEST_CASE("FacultyGuard class - set faculty guard's eligibility status for wearing gun")
+{
+  FacultyGuard facultyGuard("Dimitar Ivanov", "Varna", 1965, 35, 1777.7, true);
+  REQUIRE(facultyGuard.getGunEligibilityStatus() == true);
+}
